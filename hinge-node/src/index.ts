126bb2bc3b41f593a892edad34efac48a0ed5113
Kishore <kishore07kzr@gmail.com>
2025-02-14 20:22:58 +0530

    build: add express

--- hinge-node/src/index.ts
+++ hinge-node/src/index.ts
@@ -1,1 +1,11 @@
-console.log("Hello, world!");
+import express from "express";
+
+const app = express();
+const PORT = 3000;
+app.listen(PORT, () => {
+  console.log(`Server is running at http://localhost:${PORT}`);
+});
+
+app.get("/", (req, res) => {
+  res.send("Hello World");
+});
