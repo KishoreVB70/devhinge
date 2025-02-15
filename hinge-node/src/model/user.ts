8866667e0c12952163f1c71ed2f220f4e3afaba3
Kishore <kishore07kzr@gmail.com>
2025-02-15 10:29:14 +0530

    feat: migrate user schema to Zod for validation and update model definition

--- hinge-node/src/model/user.ts
+++ hinge-node/src/model/user.ts
@@ -1,23 +1,16 @@
-import mongoose, { Schema } from "mongoose";
+import { zodSchema } from "@zodyac/zod-mongoose";
+import { model } from "mongoose";
+import { z } from "zod";
+import { extendZod } from "@zodyac/zod-mongoose";
 
-const useSchema = new Schema({
-  name: {
-    type: String,
-    required: true,
-  },
-  email: {
-    type: String,
-    required: true,
-    isunique: true,
-  },
-  image: {
-    type: String,
-    required: true,
-  },
-  password: {
-    type: String,
-    required: true,
-  },
+extendZod(z);
+
+const zUser = z.object({
+  name: z.string().min(3).max(18),
+  email: z.string().email().unique(),
+  image: z.string().url(),
+  password: z.string().min(6).max(18),
 });
 
-export const User = mongoose.model("User", useSchema);
+const userSchema = zodSchema(zUser);
+export const User = model("User", userSchema);
