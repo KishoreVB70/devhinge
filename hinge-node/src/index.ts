6d2b8d7f8e61b9b33e78fab251224e6f15229ba8
Kishore <kishore07kzr@gmail.com>
2025-02-15 06:53:07 +0530

    build: add mongoose

--- hinge-node/src/index.ts
+++ hinge-node/src/index.ts
@@ -25,6 +25,7 @@ app.post("/user", (req, res) => {
   res.send("User created");
 });
 
+// Fallback error handler
 app.use("/", (err: any, req: Request, res: Response, next: NextFunction) => {
   if (err) {
     console.error(err);
