8adda35376785003c402848de2fb71755d9371a0
Kishore <kishore07kzr@gmail.com>
2025-02-17 14:58:57 +0530

    feat: add schema-level validation for connection creation and updates

--- hinge-node/src/model/connection.ts
+++ hinge-node/src/model/connection.ts
@@ -27,6 +27,65 @@ const zConnection = z.object({
 });
 
 const connectionSchema = zodSchema(zConnection, { timestamps: true });
+connectionSchema.index({ senderId: 1, targetId: 1 }, { unique: true });
+
+// Schema level validation for connection creation
+connectionSchema.pre("save", async function (next) {
+  // 1) Check if the connection already exists
+  const existingConnection = await Connection.findOne({
+    $or: [
+      { senderId: this.senderId, targetId: this.targetId },
+      { senderId: this.targetId, targetId: this.senderId },
+    ],
+  });
+
+  if (existingConnection) {
+    return next(new Error("Connection already exists in reverse order"));
+  }
+
+  // 2) Check if user doesn't connect to themselves
+  if (this.senderId === this.targetId) {
+    return next(new Error("User cannot connect to themselves"));
+  }
+
+  next();
+});
+
+// Schema validation for updating connection status
+connectionSchema.pre("findOneAndUpdate", async function (next) {
+  try {
+    const update = this.getUpdate();
+    const query = this.getQuery();
+
+    let newStatus: string | undefined;
+
+    if (!Array.isArray(update)) {
+      newStatus = update?.$set?.status || update?.status;
+    }
+
+    // Update must be either of "accepted" or "rejected"
+    if (!newStatus || newStatus === "interested" || newStatus === "ignore") {
+      return next(new Error("Invalid status update"));
+    }
+
+    const existingConnection = await this.model.findOne(query);
+    if (!existingConnection) {
+      return next(new Error("Connection not found"));
+    }
+
+    // Existing connection must be in "interested" status to be updated
+    if (existingConnection.status !== "interested") {
+      return next(
+        new Error("Can only accept a connection that is in 'interested' status")
+      );
+    }
+
+    next();
+  } catch (error) {
+    next(new Error("Error validating connection update in schema level"));
+  }
+});
+
 connectionSchema.path("senderId").immutable(true);
 connectionSchema.path("targetId").immutable(true);
 const Connection = model("Connection", connectionSchema);
