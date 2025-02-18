140d329287eaadbc0203390b9883c1d943b5baff
Kishore <kishore07kzr@gmail.com>
2025-02-18 16:16:32 +0530

    feat: add zod schema for auth

---
+++ hinge-next/lib/schema/zodSchema.ts
@@ -0,0 +1,23 @@
+import { z } from "zod";
+
+const passwordSchema = z
+  .string()
+  .min(8, { message: "Password must be at least 8 characters long" })
+  .max(32, { message: "Password must be at most 32 characters long" })
+  .regex(/[A-Z]/, {
+    message: "Password must contain at least one uppercase letter",
+  })
+  .regex(/[a-z]/, {
+    message: "Password must contain at least one lowercase letter",
+  })
+  .regex(/[0-9]/, { message: "Password must contain at least one number" })
+  .regex(
+    /[!@#$%^&*(),.?":{}|<>]/,
+    "Password must contain at least one special character"
+  )
+  .regex(/^\S+$/, "Password must not contain spaces");
+
+export const authSchema = z.object({
+  email: z.string().email(),
+  password: passwordSchema,
+});
