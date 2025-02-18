1917591b3bcf0ae0cfb2dfb985c00025731ed0ae
Kishore <kishore07kzr@gmail.com>
2025-02-18 07:33:28 +0530

    feat: implement feed endpoint to fetch users while excluding hidden connections and prioritizing interested users

--- hinge-node/src/index.ts
+++ hinge-node/src/index.ts
@@ -1,11 +1,14 @@
 import express, { Request, Response, NextFunction } from "express";
 import { connectDB } from "./config/mongoose.js";
-import { User } from "./model/user.js";
 import cookieParser from "cookie-parser";
 import authRouter from "./routes/auth.js";
 import userRouter from "./routes/users.js";
 
 import { authentication } from "./middleware/authenticated.js";
+import Connection from "./model/connection.js";
+import mongoose from "mongoose";
+import { User } from "./model/user.js";
+import { successResponse } from "./utils/utils.js";
 
 const app = express();
 const PORT = 3000;
@@ -30,9 +33,76 @@ async function main() {
   app.use("/", userRouter);
 
   app.get("/feed", authentication, async (req, res) => {
+    const userId = req.body.id;
     try {
-      const feed = await User.aggregate([{ $sample: { size: 10 } }]);
-      res.json(feed);
+      /*
+       *Users Not to be displayed:
+       * 1. The user itself
+       * 2. Users who are already connected
+       * 3. Users to whom the user has sent a connection request
+       * 4. Users who have ignored the user
+       * 5. Users who have rejected the user's connection request
+       */
+      /*
+       *Users to be displayed:
+       * 1. Users who have sent a connection request to the user
+       * 2. Users who have doesn't have any connection to the user
+       */
+      const HiddenConnections = await Connection.find({
+        $or: [{ senderId: userId }, { targetId: userId }],
+      })
+        .lean()
+        .select("senderId targetId status");
+
+      const hiddenUsers = new Set<string>();
+      const interestedUsers: string[] = [];
+      HiddenConnections.forEach((connection) => {
+        if (
+          connection.status === "interested" &&
+          connection.targetId.toString() === userId
+        ) {
+          interestedUsers.push(connection.senderId.toString());
+        } else {
+          hiddenUsers.add(connection.senderId.toString());
+          hiddenUsers.add(connection.targetId.toString());
+        }
+      });
+
+      const feed = await User.aggregate([
+        {
+          $match: {
+            _id: { $nin: [...hiddenUsers] }, // Exclude hidden users
+          },
+        },
+        {
+          $addFields: {
+            isInterested: {
+              $cond: {
+                if: { $in: ["$_id", interestedUsers] },
+                then: 1,
+                else: 0,
+              }, // Field to prioritize "interested" users
+            },
+          },
+        },
+        {
+          $sort: {
+            isInterested: -1,
+            _id: 1,
+          },
+        },
+        {
+          $project: {
+            isInterested: 0, // Remove isInterested field from the result
+          },
+        },
+      ]);
+
+      if (!feed) {
+        return successResponse(res, "No users found", []);
+      }
+
+      successResponse(res, "Feed fetched", feed);
     } catch (error) {
       console.error("Error fetching feed: ", error);
       res.status(500).send("Internal server error");
