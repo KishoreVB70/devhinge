9a48ef814f8c1e34436a135f56ef7bde6152ec46
Kishore <kishore07kzr@gmail.com>
2025-02-21 15:41:40 +0530

    feat: modify actions

--- hinge-next/lib/actions/loginAction.ts
+++ hinge-next/lib/actions/loginAction.ts
@@ -1,8 +1,12 @@
 "use server";
-"server only";
+"server-only";
 
 import { supabase } from "@/lib/config/supabase";
-import { authSchema } from "@/lib/schema/zodSchema";
+import { authSchema } from "@/lib/schema/authSchema";
+import { serverEnv } from "@/lib/utils/serverEnv";
+import bcrypt from "bcrypt";
+import jwt from "jsonwebtoken";
+import { cookies } from "next/headers";
 
 export default async function loginAction(formData: FormData) {
   const rawFormData = {
@@ -12,7 +16,43 @@ export default async function loginAction(formData: FormData) {
   // 1) Data sanitization
   const validatedData = authSchema.parse(rawFormData);
 
-  // 2) Add to supabase
-  const { data, error } = await supabase.auth.signInWithPassword(validatedData);
-  console.log(data, error);
+  // 2) User exists
+  const { data, error } = await supabase
+    .from("users")
+    .select("password, id")
+    .eq("email", validatedData.email);
+
+  if (error) {
+    throw new Error(error.message);
+  }
+
+  if (!data || data.length === 0) {
+    throw new Error("Invalid email or password");
+  }
+
+  const isPasswordCorrect = await bcrypt.compare(
+    validatedData.password,
+    data[0].password
+  );
+
+  if (!isPasswordCorrect) {
+    throw new Error("Invalid email or password");
+  }
+
+  // Generate jwt
+  const token = jwt.sign({ id: data[0].id }, serverEnv.JWT_SECRET, {
+    expiresIn: "1d",
+  });
+
+  const cookieStore = await cookies();
+  cookieStore.set({
+    name: "token",
+    value: token,
+    httpOnly: true,
+    sameSite: "strict",
+    path: "/",
+    maxAge: 60 * 60 * 24,
+  });
+
+  return true;
 }
--- hinge-next/lib/actions/signupAction.ts
+++ hinge-next/lib/actions/signupAction.ts
@@ -1,18 +1,30 @@
 "use server";
-"server only";
+"server-only";
 
 import { supabase } from "@/lib/config/supabase";
-import { authSchema } from "@/lib/schema/zodSchema";
+import { authSchema } from "@/lib/schema/authSchema";
+import bcrypt from "bcrypt";
 
 export default async function signupAction(formData: FormData) {
-  const rawFormData = {
-    email: formData.get("email"),
-    password: formData.get("password"),
-  };
-  // 1) Data sanitization
-  const validatedData = authSchema.parse(rawFormData);
+  try {
+    const rawFormData = {
+      email: formData.get("email"),
+      password: formData.get("password"),
+    };
+    const validatedUserData = authSchema.parse(rawFormData);
 
-  // 2) Add to supabase
-  const { data, error } = await supabase.auth.signUp(validatedData);
-  console.log(data, error);
+    const hashedPassword = await bcrypt.hash(validatedUserData.password, 10);
+
+    const userData = {
+      email: validatedUserData.email,
+      password: hashedPassword,
+    };
+
+    const { error } = await supabase.from("users").insert(userData);
+    if (error) {
+      throw new Error(error.message);
+    }
+  } catch (error) {
+    console.error(error);
+  }
 }
