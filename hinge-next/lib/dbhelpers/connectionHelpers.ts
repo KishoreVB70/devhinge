5a59a51673910b587f14231caa4909fd45fdfbf6
Kishore <kishore07kzr@gmail.com>
2025-02-22 15:59:59 +0530

    feat: add modify connection function for accepted and rejected status

--- hinge-next/lib/dbhelpers/connectionHelpers.ts
+++ hinge-next/lib/dbhelpers/connectionHelpers.ts
@@ -1,24 +1,28 @@
 import "server-only";
 import { supabase } from "@/lib/config/supabase";
-import { NewConnectionStatus } from "@/lib/schema/connectionSchema";
+import {
+  ModifyConnectionStatus,
+  NewConnectionStatus,
+} from "@/lib/schema/connectionSchema";
 
 // Helper functions
+async function userExists(userId: string) {
+  const { data, error } = await supabase
+    .from("users")
+    .select("id")
+    .eq("id", userId)
+    .single();
+  if (error) {
+    throw new Error(`Error Querying DB: ${error.message}`);
+  }
+  if (!data) {
+    throw new Error("User not found");
+  }
+}
 async function newConnectionValidation(senderId: string, targetId: string) {
   // 1) Validate target id
   // Note: Sender would be automatically valid since passed through middleware
-  const { data, error: targetError } = await supabase
-    .from("users")
-    .select("id")
-    .eq("id", targetId)
-    .single();
-
-  if (targetError) {
-    throw new Error(`Error Querying DB: ${targetError.message}`);
-  }
-
-  if (!data) {
-    throw new Error("Target user not found");
-  }
+  await userExists(targetId);
 
   // 2) Check if Connection already Exists
   const { data: connectionData, error } = await supabase
@@ -52,33 +56,47 @@ export async function createNewConnection(
   }
 }
 
-// async function modifyConnectionValidation(senderId: string, targetId: string) {
-//   // 1) Make sure the initial sender is an actual user id
-//   await userExists(senderId);
+async function modifyConnectionValidation(senderId: string, targetId: string) {
+  // 1) Make sure the initial sender is an actual user id
+  await userExists(senderId);
 
-//   // 2) Check whether Connection exists in "interested" status
-//   // Here, sender is the initial "interested" sender
-//   const connection = await Connection.findOne({
-//     senderId,
-//     targetId,
-//     status: "interested",
-//   });
+  // 2) Check whether Connection exists in "interested" status
+  // Here, sender is the initial "interested" sender
+  const { data, error } = await supabase
+    .from("connections")
+    .select("id")
+    .eq("sender_id", senderId)
+    .eq("target_id", targetId)
+    .eq("status", "interested")
+    .single();
 
-//   if (!connection) {
-//     throw new Error("Connection not found");
-//   }
-// }
-// async function modifyConnection(
-//   senderID: string,
-//   targetID: string,
-//   status: ModifyConnectionStatus
-// ) {
-//   // NOTE:  Inverse senderId and targetId for the initial "interested" sender
-//   const senderId = targetID;
-//   const targetId = senderID;
+  if (error) {
+    throw new Error(`Error Querying DB: ${error.message}`);
+  }
+  if (!data) {
+    throw new Error("Connection not found");
+  }
+}
+export async function modifyConnection(
+  senderID: string,
+  targetID: string,
+  status: ModifyConnectionStatus
+) {
+  // NOTE:  Inverse senderId and targetId for the initial "interested" sender
+  const senderId = targetID;
+  const targetId = senderID;
 
-//   await modifyConnectionValidation(senderId, targetId);
-//   const filter = { senderId, targetId };
-//   const connection = await Connection.findOneAndUpdate(filter, { status });
-//   return connection;
-// }
+  await modifyConnectionValidation(senderId, targetId);
+  const { data, error } = await supabase
+    .from("connections")
+    .update({ status })
+    .eq("sender_id", senderId)
+    .eq("target_id", targetId)
+    .eq("status", "interested");
+  if (error) {
+    throw new Error(`Error Querying DB: ${error.message}`);
+  }
+  if (!data) {
+    throw new Error("Connection not found");
+  }
+}
