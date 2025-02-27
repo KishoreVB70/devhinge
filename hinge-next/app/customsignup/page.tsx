15e0b26b86b487b6e5cc645e543ce77e9e5bb6dd
Kishore <kishore07kzr@gmail.com>
2025-02-27 15:11:56 +0530

    feat: add gender details to form with shadcn

--- hinge-next/app/customsignup/page.tsx
+++ hinge-next/app/customsignup/page.tsx
@@ -1,68 +1,159 @@
 "use client";
 import { Button } from "@/components/ui/button";
 import { customSignupAction } from "@/lib/actions/signupAction";
-import { signupInputs, zSignupInput } from "@/lib/schema/formSchema";
+import { CustomSigunpInput, zCustomSigunpInput } from "@/lib/schema/formSchema";
 import React from "react";
 import { zodResolver } from "@hookform/resolvers/zod";
 import { useForm, SubmitHandler } from "react-hook-form";
+import {
+  Form,
+  FormControl,
+  FormDescription,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form";
+import { Input } from "@/components/ui/input";
+import { RadioGroup, RadioGroupItem } from "@/components/ui/radio-group";
+import { zGender } from "@/lib/schema/userSchema";
+
+const genderOptions = Object.values(zGender.Values);
+function RadioFormItem({ value, label }: { value: string; label: string }) {
+  return (
+    <FormItem className="flex items-center space-x-3 space-y-0">
+      <FormControl>
+        <RadioGroupItem value={value} />
+      </FormControl>
+      <FormLabel className="font-normal">{label}</FormLabel>
+    </FormItem>
+  );
+}
 
 function Signup() {
-  const {
-    register,
-    handleSubmit,
-    formState: { errors, isSubmitting },
-    setError,
-    reset,
-  } = useForm<signupInputs>({
-    resolver: zodResolver(zSignupInput),
+  const form = useForm<CustomSigunpInput>({
+    defaultValues: {
+      email: "",
+      password: "",
+      name: "",
+      age: 10,
+      gender: "male",
+    },
+    resolver: zodResolver(zCustomSigunpInput),
   });
-  // Required information for signup:
-  // 1) Email
-  // 2) Password
 
-  // 3) Username
-  // 4) Age
-
-  const onsubmit: SubmitHandler<signupInputs> = async (data) => {
+  const onsubmit: SubmitHandler<CustomSigunpInput> = async (data) => {
     const formData = new FormData();
     formData.append("email", data.email);
     formData.append("password", data.password);
     const error = await customSignupAction(data);
     if (error) {
-      setError("root", { message: error });
-    } else {
-      reset();
+      console.log(error);
     }
   };
 
   return (
     <div className="flex flex-row items-center justify-center h-screen">
-      <form onSubmit={handleSubmit(onsubmit)}>
-        <div>
-          <label htmlFor="email">Email:</label>
-          <input
-            className="border border-black"
-            {...register("email", { required: true })}
+      <Form {...form}>
+        <form
+          onSubmit={form.handleSubmit(onsubmit)}
+          className=" min-w-[500px] space-y-2"
+        >
+          {/* Username */}
+          <FormField
+            control={form.control}
+            name="name"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>Username</FormLabel>
+                <FormControl>
+                  <Input placeholder="John Doe" {...field} />
+                </FormControl>
+                <FormDescription>
+                  This is your public display name.
+                </FormDescription>
+                <FormMessage />
+              </FormItem>
+            )}
           />
-          {errors.email && (
-            <p className="text-red-700">{errors.email.message}</p>
-          )}
-        </div>
-        <div className="mt-3">
-          <label htmlFor="password">Password:</label>
-          <input
-            className="border border-black"
-            {...register("password", { required: true })}
+          {/* Email */}
+          <FormField
+            control={form.control}
+            name="email"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>Email</FormLabel>
+                <FormControl>
+                  <Input placeholder="Johndoe@email.com" {...field} />
+                </FormControl>
+                <FormDescription>
+                  Your email will be used to login.
+                </FormDescription>
+                <FormMessage />
+              </FormItem>
+            )}
           />
-          {errors.password && (
-            <p className="text-red-700">{errors.password.message}</p>
-          )}
-        </div>
-
-        <Button type="submit" disabled={isSubmitting}>
-          {isSubmitting ? "loading" : "Submit"}
-        </Button>
-      </form>
+          {/* Password */}
+          <FormField
+            control={form.control}
+            name="password"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>Password</FormLabel>
+                <FormControl>
+                  <Input {...field} />
+                </FormControl>
+                <FormDescription>
+                  Password must contain 8 characters
+                </FormDescription>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+          {/* Age */}
+          <FormField
+            control={form.control}
+            name="age"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>Age</FormLabel>
+                <FormControl>
+                  <Input type="number" {...field} />
+                </FormControl>
+                <FormDescription>
+                  Minium age to use the app is 10
+                </FormDescription>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+          {/* Gender */}
+          <FormField
+            control={form.control}
+            name="gender"
+            render={({ field }) => (
+              <FormItem className="space-y-3">
+                <FormLabel>Gender</FormLabel>
+                <FormControl>
+                  <RadioGroup
+                    onValueChange={field.onChange}
+                    defaultValue={field.value}
+                    className="flex flex-col space-y-1"
+                  >
+                    {genderOptions.map((item) => (
+                      <RadioFormItem key={item} value={item} label={item} />
+                    ))}
+                  </RadioGroup>
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+          <Button type="submit" disabled={form.formState.isSubmitting}>
+            {form.formState.isSubmitting ? "loading" : "Submit"}
+          </Button>
+        </form>
+      </Form>
     </div>
   );
 }
--- hinge-next/lib/schema/formSchema.ts
+++ hinge-next/lib/schema/formSchema.ts
@@ -1,5 +1,17 @@
+import { zUser } from "@/lib/schema/userSchema";
 import { z } from "zod";
 
+export const zCustomSigunpInput = zUser
+  .pick({
+    email: true,
+    password: true,
+    age: true,
+    name: true,
+    gender: true,
+  })
+  .required();
+
+export type CustomSigunpInput = z.infer<typeof zCustomSigunpInput>;
 export const zSignupInput = z.object({
   email: z.string().email(),
   password: z.string().min(8),
