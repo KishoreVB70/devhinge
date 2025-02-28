a0136fc2ecf4b2cc42ecdcbf9014fc50c13e54d0
Kishore <kishore07kzr@gmail.com>
2025-02-28 14:20:31 +0530

    style: change layout

--- hinge-next/app/signup/page.tsx
+++ hinge-next/app/signup/page.tsx
@@ -2,7 +2,7 @@ import SignUpFormWrapper from "@/components/auth/signup/SignupForm";
 
 function Signup() {
   return (
-    <div className="flex flex-row items-center justify-center h-screen">
+    <div className="flex flex-row items-center justify-center h-full">
       <SignUpFormWrapper />
     </div>
   );
