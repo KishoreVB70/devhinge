398ad50344f47e7e5670aad90e03421cd6f4f23b
Kishore <kishore07kzr@gmail.com>
2025-03-13 11:41:08 +0530

    refactor: change edit profile path to /profile

--- hinge-next/app/profile/page.tsx
+++ hinge-next/app/profile/page.tsx
@@ -10,7 +10,7 @@ export default async function EditProfile() {
 
   return (
     <div className="flex flex-col items-center justify-center">
-      <EditProfileForm user={user} />{" "}
+      <EditProfileForm user={user} />
     </div>
   );
 }
