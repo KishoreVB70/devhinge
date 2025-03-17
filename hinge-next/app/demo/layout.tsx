8d147ba872f11e340c9c6598138343dd62eb7f9a
Kishore <kishore07kzr@gmail.com>
2025-03-17 13:49:05 +0530

    feat: add demo path

---
+++ hinge-next/app/demo/layout.tsx
@@ -0,0 +1,24 @@
+import SideBar from "@/components/SideBarWrapper";
+import Image from "next/image";
+import React, { Suspense } from "react";
+
+export default function Layout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <div className="h-screen w-screen overflow-hidden">
+      {/* Only on Mobile layout */}
+      <div className="relative h-[4%] block lg:hidden w-[115px] ml-2 my-2">
+        <Image fill src="/tindertext.png" alt="logo" />
+      </div>
+      <div className="h-[96%] lg:h-full flex w-full">
+        <Suspense fallback={<div>Loading...</div>}>
+          <SideBar demo />
+        </Suspense>
+        <Suspense fallback={<div>Loading...</div>}>{children}</Suspense>
+      </div>
+    </div>
+  );
+}
