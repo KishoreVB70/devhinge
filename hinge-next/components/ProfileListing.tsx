a39f8cda25ffb004b85b29a39e88111a4388744b
Kishore <kishore07kzr@gmail.com>
2025-03-17 11:26:09 +0530

    add ProfileListing component to display connected profiles

---
+++ hinge-next/components/ProfileListing.tsx
@@ -0,0 +1,27 @@
+import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
+import { SimpleProfile } from "@/lib/schema/userSchema";
+import Link from "next/link";
+import React from "react";
+type ProfileListingProps = {
+  profile: SimpleProfile;
+};
+function ProfileListing({ profile }: ProfileListingProps) {
+  return (
+    <Link
+      href={`/user/${profile.id}`}
+      className="py-2 space-x-2 cursor-pointer flex flex-row items-center shadow-lg border border-gray-100"
+    >
+      <Avatar className="ml-2 h-16 w-16">
+        <AvatarImage
+          src={profile.avatar_url}
+          alt="avatar"
+          className="object-cover"
+        />
+        <AvatarFallback>DC</AvatarFallback>
+      </Avatar>
+      <h1 className="font-semibold text-black text-2xl">{profile.name}</h1>
+    </Link>
+  );
+}
+
+export default ProfileListing;
