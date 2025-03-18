d23be8c6275818bd9806c206bdf94002aabe36eb
Kishore <kishore07kzr@gmail.com>
2025-03-18 08:37:37 +0530

    feat: modify middleware to add x-isdemo to the headers

--- hinge-next/middleware.ts
+++ hinge-next/middleware.ts
@@ -13,14 +13,25 @@ export async function middleware(req: NextRequest) {
     const { payload } = await jwtVerify(token, secret);
     const response = NextResponse.next();
     response.headers.set("id", payload.id as string);
+    const isDemo = req.nextUrl.pathname.includes("/demo");
+    response.headers.set("x-isdemo", isDemo.toString());
+
     return response;
   } catch (error) {
     console.error(error);
     return NextResponse.redirect(new URL("/auth/signin", req.url));
   }
 }
+
+/*Don't match
+/auth
+unique-email
+unique-username
+_next/image
+favicon.ico
+_next/static
+ */
+
 export const config = {
-  matcher: [
-    "/((?!^$|auth/signin|auth/signup|unique-email|unique-username|test/login$|test/users$|_next/static|_next/image|favicon.ico).*)",
-  ],
+  matcher: ["/((?!_next/static|_next/image|favicon.ico).*)"],
 };
